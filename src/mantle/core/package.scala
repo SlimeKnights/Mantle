e6b8e035eb2517e991b03b7ddc6d5ef65bf85832
Sunstrike <sunstrike@azurenode.net>
2013-10-28 22:49:06 +0000

    Package objects for documenting the major modules (Core + Router)

---
+++ src/mantle/core/package.scala
@@ -0,0 +1,11 @@
+package mantle
+
+/**
+ * Mantle Core
+ *
+ * Provides the base framework for all of the other Mantle modules in addition
+ * to some basic functions of it's own.
+ *
+ * @author Sunstrike <sunstrike@azurenode.net>
+ */
+package object core {}
---
+++ src/mantle/router/package.scala
@@ -0,0 +1,15 @@
+package mantle
+
+/**
+ * Mantle Router
+ *
+ * Provides advanced IMC message routing functionality to other mods for more convenient
+ * registration with external modules.
+ *
+ * Planned modules/helpers:
+ *  * BuildCraft Transport (Facades)
+ *  * Forge Multipart (Microblocks)
+ *  * Fluid blacklist helper (Mystcraft et al.)
+ *  * TConstruct dimension blacklisting
+ */
+package object router {}
