ab5c3e34c3d6b9bf97345a2fb1bbd787bad21054
Sunstrike <sunstrike@azurenode.net>
2013-11-03 11:23:05 +0000

    BC Transport plugin + unit

---
+++ src/mantle/router/routing/builtin/UnitBCTransport.scala
@@ -0,0 +1,39 @@
+package mantle.router.routing.builtin
+
+import mantle.router.routing.{MantleMessage, IPipelineUnit}
+import mantle.router.lib.RouterRepo._
+import cpw.mods.fml.common.event.FMLInterModComms
+
+/**
+ * Buildcraft Transport pipeline unit
+ *
+ * @author Sunstrike <sunstrike@azurenode.net>
+ */
+object UnitBCTransport extends IPipelineUnit {
+  /**
+   * Getter for the name of this unit
+   *
+   * @example def unitName: String = "FMP Compat"
+   * @return The name of this unit (something descriptive)
+   */
+  def unitName = "BC Transport Compat"
+
+  /**
+   * Handles an incoming Mantle message
+   *
+   * @param msg The Mantle message from Router.
+   */
+  def handleMessage(msg: MantleMessage) = msg.getAttachment match {
+    case (bID:Int, meta:Int) => sendBCFacadeMessage(bID, meta)
+    case il:Array[Int]       => try { sendBCFacadeMessage(il(0), il(1)) } catch { case e:Exception => logWarn("Invalid Int array received.") }
+  }
+
+  private def sendBCFacadeMessage(bID: Int, meta: Int) {
+    FMLInterModComms.sendMessage("BuildCraft|Transport", "add-facade", s"$bID@$meta")
+  }
+
+  private def logWarn(str:String) {
+    logger.warning(s"[$unitName] $str")
+  }
+
+}
