b090f0a0a0948d6427b923ed4bd31e60bced201a
Sunstrike <sunstrike@azurenode.net>
2013-11-03 11:22:47 +0000

    Remove erroneous logging in AE Unit

--- src/mantle/router/routing/builtin/UnitAppEng.scala
+++ src/mantle/router/routing/builtin/UnitAppEng.scala
@@ -1,7 +1,6 @@
 package mantle.router.routing.builtin
 
 import mantle.router.routing.{MantleMessage, IPipelineUnit}
-import mantle.router.lib.RouterRepo._
 import cpw.mods.fml.common.event.FMLInterModComms
 
 /**
@@ -15,7 +14,6 @@ object UnitAppEng extends IPipelineUnit {
 
   def handleMessage(msg: MantleMessage) = msg.getAttachment match {
     case s:String => FMLInterModComms.sendMessage("AppliedEnergistics", "movabletile", s)
-    case _        => logger.warning(s"Received invalid movableTile message: $msg")
   }
 
 }
