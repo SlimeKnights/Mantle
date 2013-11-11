7b3ad29ba02697c31ec3561260997c20856f492d
Sunstrike <sunstrike@azurenode.net>
2013-11-11 23:08:04 +0000

    Java wrappers for min/max
    Silly Java can't use type-classed code.

--- src/mantle/core/math/MathUtils.scala
+++ src/mantle/core/math/MathUtils.scala
@@ -27,4 +27,34 @@ object MathUtils {
    */
   def max[T <% Ordered[T]](x:T, y:T):T = if (x > y) x else y
 
+  /**
+   * Java helper for min (Integers)
+   */
+  def minInt(x:Int, y:Int) = min(x, y)
+
+  /**
+   * Java helper for max (Integers)
+   */
+  def maxInt(x:Int, y:Int) = max(x, y)
+
+  /**
+   * Java helper for min (Floats)
+   */
+  def minFloat(x:Float, y:Float) = min(x, y)
+
+  /**
+   * Java helper for max (Floats)
+   */
+  def maxFloat(x:Float, y:Float) = max(x, y)
+
+  /**
+   * Java helper for min (Doubles)
+   */
+  def minDouble(x:Double, y:Double) = min(x, y)
+
+  /**
+   * Java helper for max (Doubles)
+   */
+  def maxDouble(x:Double, y:Double) = max(x, y)
+
 }
