94b1983cf633eae2c640fa1d44eeece29fd6eeb2
Sunstrike <sun@sunstrike.io>
2014-01-06 20:51:08 +0000

    Update PlayerUtils.java
    Misc formatting and javadocs.

--- src/mantle/player/PlayerUtils.java
+++ src/mantle/player/PlayerUtils.java
@@ -3,11 +3,21 @@ package mantle.player;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.util.ChatComponentText;
 
+/**
+ * Misc utils for dealing with players.
+ * 
+ * @author progwml6
+ */
 public class PlayerUtils
 {
+    
+    /**
+     * Sends a given message to a specific player (due to 1.7 axing addChatMessage())
+     */
     public static void sendChatMessage (EntityPlayer p, String message)
     {
         if (p != null && message != null && message.isEmpty())
             p.func_146105_b(new ChatComponentText(message));
     }
+    
 }
