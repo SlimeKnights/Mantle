0a19cc31ccd34f4b3e40c052d731ece5c8482832
Sunstrike <sun@sunstrike.io>
2014-04-19 16:52:08 +0100

    Initial implementation of the v2 module loader.

---
+++ src/mantle/module/IModule.java
@@ -0,0 +1,25 @@
+package mantle.module;
+
+/**
+ * Interface for modules for dealing with other mods.
+ *
+ * Note that all IModule's should have a static String field "modId" which contains the mod ID of the mod it supports.
+ * This is accessed by reflection because Java is dumb and has no statics in interfaces.
+ *
+ * @author Arkan <arkan@emberwalker.cc>
+ */
+public interface IModule {
+
+    // Example modId field
+    //public static String modId = "ExampleMod";
+
+    // Called during PreInit
+    public abstract void preInit();
+
+    // Called during Init
+    public abstract void init();
+
+    // Called during PostInit
+    public abstract void postInit();
+
+}
