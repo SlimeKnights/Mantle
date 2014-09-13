02ac64943c53ac5a2ed526b91159a6d56ab20aaa
squeek <squeek502@hotmail.com>
2014-09-12 19:01:03 -0700

    Revert "fix issue with remap event not applying mod containers correctly by checking full remap list"
    This reverts commit a1dc18fa66ae0a419bf9187215be3188a42648f4.

--- src/mantle/event/Migration.java
+++ src/mantle/event/Migration.java
@@ -63,13 +63,12 @@ public abstract class Migration
      * During this phase, check for old IDs and remap as needed, or destroy ones marked for removal.
      */
     public void processMigrationEvent (FMLMissingMappingsEvent event) {
-        if ((!remapBlock.isEmpty() || !remapItem.isEmpty()))
-        {
-            List<MissingMapping> l = event.getAll();
+        if ((!remapBlock.isEmpty() || !remapItem.isEmpty())) {
+            event.applyModContainer(mod);
+            List<MissingMapping> l = event.get();
             if(l != null)
             {
-                for (MissingMapping miss : event.getAll())//yes its slow to go through extra stuff feel free to fix
-                {
+                for (MissingMapping miss : event.get()) {
                     if (remapItem.containsKey(miss.name))
                         miss.remap(remapItem.get(miss.name));
                     else if (remapBlock.containsKey(miss.name))
