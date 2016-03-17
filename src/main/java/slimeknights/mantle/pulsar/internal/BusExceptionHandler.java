3adbcf303343f4bfcd67ed42322ccbf452b562a5
Bernhard Bonigl <bernhard.bonigl@gmail.com>
2016-03-17 13:43:51 +0100

    Crash on exception during startup

--- src/main/java/slimeknights/mantle/pulsar/internal/BusExceptionHandler.java
+++ src/main/java/slimeknights/mantle/pulsar/internal/BusExceptionHandler.java
@@ -27,9 +27,7 @@ public final class BusExceptionHandler implements IExceptionHandler {
     @Override
     public void handle(Exception ex) {
         this.logger.error("Exception caught from a pulse on flightpath for mod ID " + id + ": ", ex);
-        if(ex instanceof RuntimeException) {
-            throw (RuntimeException)ex;
-        }
+        throw new Error(ex);
     }
 
     @Override
