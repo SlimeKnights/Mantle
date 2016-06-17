4bdb7b75b420264b1b955e9022212822c982935c
Bernhard Bonigl <bernhard.bonigl@gmail.com>
2016-06-17 20:12:43 +0200

    Version bump to 0.10.2

--- src/main/java/slimeknights/mantle/Mantle.java
+++ src/main/java/slimeknights/mantle/Mantle.java
@@ -20,7 +20,7 @@ import slimeknights.mantle.common.CommonProxy;
 @Mod(modid = Mantle.modId,
     name = Mantle.modName,
     version = Mantle.modVersion,
-    dependencies = "required-after:Forge@[12.16.0.1850,)",
+    dependencies = "required-after:Forge@[12.17.0.1937,)",
     acceptedMinecraftVersions = "[1.9.4,1.10)")
 public class Mantle {
 
