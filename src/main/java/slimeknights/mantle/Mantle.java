a5b7f032cf57960dfe0e1e5f5fab74b69d7cf345
Bernhard Bonigl <bernhard.bonigl@gmail.com>
2016-10-17 18:48:37 +0200

    Add the extra heart rendering from 1.7 TiC

--- src/main/java/slimeknights/mantle/Mantle.java
+++ src/main/java/slimeknights/mantle/Mantle.java
@@ -1,13 +1,16 @@
 package slimeknights.mantle;
 
+import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.Instance;
 import net.minecraftforge.fml.common.SidedProxy;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import slimeknights.mantle.client.ExtraHeartRenderHandler;
 import slimeknights.mantle.common.CommonProxy;
 
 /**
@@ -40,5 +43,12 @@ public class Mantle {
   @Mod.EventHandler
   public void preInit(FMLPreInitializationEvent event){
     proxy.preInit();
+  }
+
+  @Mod.EventHandler
+  public void postInit(FMLPostInitializationEvent event) {
+    if(event.getSide().isClient()) {
+      MinecraftForge.EVENT_BUS.register(new ExtraHeartRenderHandler());
+    }
   }
 }
