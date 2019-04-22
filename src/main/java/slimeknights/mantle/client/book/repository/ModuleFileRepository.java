e65a173890e0b275dd9425c009cb7e9e3a80fa95
KnightMiner <knightminer4@gmail.com>
2019-04-22 09:57:09 -0500

    Allow isPulseLoaded to check if pulses from other mods are loaded
    Also adds a static isPulseLoaded method so you can check if a pulse is loaded without registering a pulse manager
    If you call ModuleFileRepository with no pulse manager, it now uses the global check for pulse loaded

--- src/main/java/slimeknights/mantle/client/book/repository/ModuleFileRepository.java
+++ src/main/java/slimeknights/mantle/client/book/repository/ModuleFileRepository.java
@@ -2,6 +2,7 @@ package slimeknights.mantle.client.book.repository;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
 import slimeknights.mantle.client.book.BookLoader;
@@ -12,17 +13,22 @@ import slimeknights.mantle.pulsar.control.PulseManager;
 
 public class ModuleFileRepository extends FileRepository {
 
-    private PulseManager manager;
+    private Predicate<String> manager;
 	public ModuleFileRepository(PulseManager manager, String location) {
 		super(location);
-		this.manager = manager;
+		this.manager = manager::isPulseLoaded;
 	}
+
+    public ModuleFileRepository(String location) {
+        super(location);
+        this.manager = PulseManager::isPulseLoadedGlobal;
+    }
 
 	@Override
 	public List<SectionData> getSections() {
 		// same as super, except we remove any where the related module is not loaded
 		return Arrays.stream(BookLoader.GSON.fromJson(resourceToString(getResource(getResourceLocation("index.json"))), SectionDataModule[].class))
-				.filter((section)->section.module.isEmpty() || manager.isPulseLoaded(section.module))
+				.filter((section)->section.module.isEmpty() || manager.test(section.module))
 				.collect(Collectors.toList());
 	}
 }
