fabf401233bd31a7010ee385cb033792339d0b53
KnightMiner <knightminer4@gmail.com>
2020-07-22 15:57:22 -0500

    Replace mantle creative tab with supplier item group
    Modern item groups lazy load the item, and supplier makes it more deferred registration

---
+++ src/main/java/slimeknights/mantle/util/SupplierItemGroup.java
@@ -0,0 +1,33 @@
+package slimeknights.mantle.util;
+
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemStack;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
+
+import java.util.function.Supplier;
+
+/**
+ * Item group that sets its item based on an item supplier
+ */
+public class SupplierItemGroup extends ItemGroup {
+  private final Supplier<ItemStack> supplier;
+
+  /**
+   * Creates a new item group
+   * @param modId     Tab owner mod ID
+   * @param name      Tab name
+   * @param supplier  Item stack supplier
+   */
+  public SupplierItemGroup(String modId, String name, Supplier<ItemStack> supplier) {
+    super(String.format("%s.%s", modId, name));
+    this.setTabPath(String.format("%s/%s", modId, name));
+    this.supplier = supplier;
+  }
+
+  @OnlyIn(Dist.CLIENT)
+  @Override
+  public ItemStack createIcon() {
+    return supplier.get();
+  }
+}
