SlimeKnights/Mantle

commit f1037ce21a64ca4bcf66d03750ec0dfb31afaa4e

Add package info and fix nullability
Everything defaults to nonnull now, and is labeled where null is likely

src/main/java/slimeknights/mantle/client/book/BookLoader.java
@@ -13,13 +13,25 @@ import net.minecraftforge.fml.ModLoadingContext;
 import slimeknights.mantle.client.book.action.StringActionProcessor;
 import slimeknights.mantle.client.book.action.protocol.ProtocolGoToPage;
 import slimeknights.mantle.client.book.data.BookData;
-import slimeknights.mantle.client.book.data.content.*;
+import slimeknights.mantle.client.book.data.content.ContentBlank;
+import slimeknights.mantle.client.book.data.content.ContentBlockInteraction;
+import slimeknights.mantle.client.book.data.content.ContentCrafting;
+import slimeknights.mantle.client.book.data.content.ContentImage;
+import slimeknights.mantle.client.book.data.content.ContentImageText;
+import slimeknights.mantle.client.book.data.content.ContentSmelting;
+import slimeknights.mantle.client.book.data.content.ContentSmithing;
+import slimeknights.mantle.client.book.data.content.ContentStructure;
+import slimeknights.mantle.client.book.data.content.ContentText;
+import slimeknights.mantle.client.book.data.content.ContentTextImage;
+import slimeknights.mantle.client.book.data.content.ContentTextLeftImage;
+import slimeknights.mantle.client.book.data.content.ContentTextRightImage;
+import slimeknights.mantle.client.book.data.content.PageContent;
 import slimeknights.mantle.client.book.data.deserializer.HexStringDeserializer;
 import slimeknights.mantle.client.book.repository.BookRepository;
 import slimeknights.mantle.network.BookNetwork;
 import slimeknights.mantle.network.book.UpdateSavedPagePacket;
 
-import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
 import java.util.HashMap;
 
 @OnlyIn(Dist.CLIENT)
@@ -117,7 +129,7 @@ public class BookLoader implements IResourceManagerReloadListener {
     return info;
   }
 
-  public static void updateSavedPage(PlayerEntity player, ItemStack item, String page) {
+  public static void updateSavedPage(@Nullable PlayerEntity player, ItemStack item, String page) {
     if (player == null) {
       return;
     }
@@ -133,7 +145,7 @@ public class BookLoader implements IResourceManagerReloadListener {
    * Reloads all the books, called when the resource manager reloads, such as when the resource pack or the language is changed
    */
   @Override
-  public void onResourceManagerReload(@Nonnull IResourceManager resourceManager) {
+  public void onResourceManagerReload(IResourceManager resourceManager) {
     books.forEach((s, bookData) -> bookData.reset());
   }
 }

src/main/java/slimeknights/mantle/client/book/BookTransformer.java
@@ -8,6 +8,7 @@ import slimeknights.mantle.client.book.data.content.ContentTableOfContents;
 import slimeknights.mantle.client.book.data.element.TextData;
 import slimeknights.mantle.client.screen.book.BookScreen;
 
+import javax.annotation.Nullable;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -41,7 +42,7 @@ public abstract class BookTransformer {
     public void transform(BookData book) {
       SectionData index = new SectionData(true) {
         @Override
-        public void update(BookScreen.AdvancementCache advancementCache) {
+        public void update(@Nullable BookScreen.AdvancementCache advancementCache) {
           this.pages.clear();
 
           List<SectionData> visibleSections = this.parent.getVisibleSections(advancementCache);

src/main/java/slimeknights/mantle/client/book/data/BookData.java
@@ -15,7 +15,6 @@ import slimeknights.mantle.client.book.data.element.ItemStackData;
 import slimeknights.mantle.client.book.repository.BookRepository;
 import slimeknights.mantle.client.screen.book.BookScreen;
 
-import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
@@ -198,11 +197,13 @@ public class BookData implements IDataItem {
     Mantle.logger.info("Finished loading book");
   }
 
+  @Nullable
   public SectionData findSection(String name) {
     return this.findSection(name, null);
   }
 
-  public SectionData findSection(String name, BookScreen.AdvancementCache advancementCache) {
+  @Nullable
+  public SectionData findSection(String name, @Nullable BookScreen.AdvancementCache advancementCache) {
     for (SectionData section : this.sections) {
       section.update(advancementCache);
 
@@ -237,10 +238,12 @@ public class BookData implements IDataItem {
     return -1;
   }
 
+  @Nullable
   public PageData findPage(int number) {
     return this.findPage(number, null);
   }
 
+  @Nullable
   public PageData findPage(int number, @Nullable BookScreen.AdvancementCache advancementCache) {
     if (number < 0) {
       return null;
@@ -265,10 +268,12 @@ public class BookData implements IDataItem {
     return null;
   }
 
+  @Nullable
   public PageData findPage(String location) {
     return this.findPage(location, null);
   }
 
+  @Nullable
   public PageData findPage(String location, @Nullable BookScreen.AdvancementCache advancementCache) {
     return this.findPage(this.findPageNumber(location, advancementCache));
   }
@@ -363,7 +368,7 @@ public class BookData implements IDataItem {
     return out != null ? out : string;
   }
 
-  public void openGui(@Nonnull ITextComponent title, @Nullable ItemStack item) {
+  public void openGui(ITextComponent title, @Nullable ItemStack item) {
     if (!this.initialized) {
       this.load();
     }
@@ -372,13 +377,13 @@ public class BookData implements IDataItem {
     }
   }
 
-  public void addRepository(BookRepository repository) {
+  public void addRepository(@Nullable BookRepository repository) {
     if (repository != null && !this.repositories.contains(repository)) {
       this.repositories.add(repository);
     }
   }
 
-  public void addTransformer(BookTransformer transformer) {
+  public void addTransformer(@Nullable BookTransformer transformer) {
     if (transformer != null && !this.transformers.contains(transformer)) {
       this.transformers.add(transformer);
     }

src/main/java/slimeknights/mantle/client/book/repository/DummyRepository.java
@@ -2,26 +2,26 @@ package slimeknights.mantle.client.book.repository;
 
 import net.minecraft.resources.IResource;
 import net.minecraft.util.ResourceLocation;
-
-import java.util.List;
-
 import slimeknights.mantle.client.book.data.SectionData;
+
+import java.util.Collections;
+import java.util.List;
 
 public class DummyRepository extends BookRepository {
 
   @Override
   public List<SectionData> getSections() {
-    return null;
+    return Collections.emptyList();
   }
 
   @Override
   public ResourceLocation getResourceLocation(String path, boolean safe) {
-    return null;
+    throw new UnsupportedOperationException();
   }
 
   @Override
   public IResource getResource(ResourceLocation loc) {
-    return null;
+    throw new UnsupportedOperationException();
   }
 
   @Override
@@ -31,6 +31,6 @@ public class DummyRepository extends BookRepository {
 
   @Override
   public String resourceToString(IResource resource, boolean skipCommments) {
-    return null;
+    return "";
   }
 }

commit 8eb2932f862277bb8fa012a42b5fd65a3dd1b158

Fix book exceptions introduced in f1037ce and refined book nullability

src/main/java/slimeknights/mantle/client/book/BookHelper.java
@@ -2,15 +2,14 @@ package slimeknights.mantle.client.book;
 
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.CompoundNBT;
-import net.minecraft.nbt.INBT;
 
-import java.util.Arrays;
+import java.util.Objects;
 
 public class BookHelper {
 
   public static String getSavedPage(ItemStack item) {
     if (!item.isEmpty() && item.hasTag()) {
-      CompoundNBT mantleBook = item.getTag().getCompound("mantle").getCompound("book");
+      CompoundNBT mantleBook = Objects.requireNonNull(item.getTag()).getCompound("mantle").getCompound("book");
 
       if (mantleBook.contains("page", 8)) {
         return mantleBook.getString("page");

src/main/java/slimeknights/mantle/client/book/BookLoader.java
@@ -5,11 +5,12 @@ import com.google.gson.GsonBuilder;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.resources.IResourceManager;
-import net.minecraft.resources.IResourceManagerReloadListener;
 import net.minecraft.util.Hand;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
 import net.minecraftforge.fml.ModLoadingContext;
+import net.minecraftforge.resource.IResourceType;
+import net.minecraftforge.resource.ISelectiveResourceReloadListener;
 import slimeknights.mantle.client.book.action.StringActionProcessor;
 import slimeknights.mantle.client.book.action.protocol.ProtocolGoToPage;
 import slimeknights.mantle.client.book.data.BookData;
@@ -33,9 +34,10 @@ import slimeknights.mantle.network.packet.UpdateSavedPagePacket;
 
 import javax.annotation.Nullable;
 import java.util.HashMap;
+import java.util.function.Predicate;
 
 @OnlyIn(Dist.CLIENT)
-public class BookLoader implements IResourceManagerReloadListener {
+public class BookLoader implements ISelectiveResourceReloadListener {
 
   /** GSON object to be used for book loading purposes */
   public static final Gson GSON = new GsonBuilder().registerTypeAdapter(int.class, new HexStringDeserializer())
@@ -88,6 +90,7 @@ public class BookLoader implements IResourceManagerReloadListener {
    * @param name The name of the page type
    * @return The class of the page type, ContentError.class if page type not registered
    */
+  @Nullable
   public static Class<? extends PageContent> getPageType(String name) {
     return typeToContentMap.get(name);
   }
@@ -145,7 +148,7 @@ public class BookLoader implements IResourceManagerReloadListener {
    * Reloads all the books, called when the resource manager reloads, such as when the resource pack or the language is changed
    */
   @Override
-  public void onResourceManagerReload(IResourceManager resourceManager) {
+  public void onResourceManagerReload(IResourceManager resourceManager, Predicate<IResourceType> resourcePredicate) {
     books.forEach((s, bookData) -> bookData.reset());
   }
 }

src/main/java/slimeknights/mantle/client/book/BookTransformer.java
@@ -23,6 +23,7 @@ public abstract class BookTransformer {
     return ContentTableTransformer.INSTANCE;
   }
 
+  @SuppressWarnings("unused") // API
   public static BookTransformer contentTableTransformerForSection(String sectionName) {
     return new ContentTableTransformer(sectionName);
   }

src/main/java/slimeknights/mantle/client/book/data/BookData.java
@@ -3,6 +3,7 @@ package slimeknights.mantle.client.book.data;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.FontRenderer;
 import net.minecraft.item.ItemStack;
+import net.minecraft.resources.IResource;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.text.ITextComponent;
 import net.minecraftforge.api.distmarker.Dist;
@@ -38,7 +39,7 @@ public class BookData implements IDataItem {
 
   protected final transient ArrayList<BookTransformer> transformers = new ArrayList<>();
 
-  private ArrayList<BookRepository> repositories;
+  private final ArrayList<BookRepository> repositories;
 
   public BookData(BookRepository... repositories) {
     this.repositories = new ArrayList<>(Arrays.asList(repositories));
@@ -111,20 +112,23 @@ public class BookData implements IDataItem {
 
         if (repo.resourceExists(languageLocation)) {
           try {
-            BufferedReader br = new BufferedReader(new InputStreamReader(repo.getResource(languageLocation)
-                    .getInputStream(), StandardCharsets.UTF_8));
+            IResource resource = repo.getResource(languageLocation);
+            if (resource != null) {
+              BufferedReader br = new BufferedReader(new InputStreamReader(resource.getInputStream(),
+                      StandardCharsets.UTF_8));
 
-            String next = br.readLine();
+              String next = br.readLine();
 
-            while (next != null) {
-              if (!next.startsWith("//") && next.contains("=")) {
-                String key = next.substring(0, next.indexOf('='));
-                String value = next.substring(next.indexOf('=') + 1);
+              while (next != null) {
+                if (!next.startsWith("//") && next.contains("=")) {
+                  String key = next.substring(0, next.indexOf('='));
+                  String value = next.substring(next.indexOf('=') + 1);
 
-                this.strings.put(key, value);
+                  this.strings.put(key, value);
+                }
+
+                next = br.readLine();
               }
-
-              next = br.readLine();
             }
           }
           catch (Exception ignored) {
@@ -215,10 +219,6 @@ public class BookData implements IDataItem {
     return null;
   }
 
-  public int getFirstPageNumber(SectionData section) {
-    return this.getFirstPageNumber(section, null);
-  }
-
   public int getFirstPageNumber(SectionData section, @Nullable BookScreen.AdvancementCache advancementCache) {
     int pages = 0;
     for (SectionData sect : this.sections) {
@@ -236,11 +236,6 @@ public class BookData implements IDataItem {
     }
 
     return -1;
-  }
-
-  @Nullable
-  public PageData findPage(int number) {
-    return this.findPage(number, null);
   }
 
   @Nullable
@@ -268,14 +263,10 @@ public class BookData implements IDataItem {
     return null;
   }
 
-  @Nullable
-  public PageData findPage(String location) {
-    return this.findPage(location, null);
-  }
-
+  @SuppressWarnings("unused") // API
   @Nullable
   public PageData findPage(String location, @Nullable BookScreen.AdvancementCache advancementCache) {
-    return this.findPage(this.findPageNumber(location, advancementCache));
+    return this.findPage(this.findPageNumber(location, advancementCache), advancementCache);
   }
 
   public int findPageNumber(String location) {
@@ -319,10 +310,6 @@ public class BookData implements IDataItem {
     return -1;
   }
 
-  public int getPageCount() {
-    return this.getPageCount(null);
-  }
-
   public int getPageCount(@Nullable BookScreen.AdvancementCache advancementCache) {
     int pages = 0;
     for (SectionData section : this.sections) {
@@ -331,10 +318,6 @@ public class BookData implements IDataItem {
       pages += section.isUnlocked(advancementCache) ? section.getPageCount() : 0;
     }
     return pages;
-  }
-
-  public int getFullPageCount() {
-    return this.getFullPageCount(null);
   }
 
   public int getFullPageCount(@Nullable BookScreen.AdvancementCache advancementCache) {
@@ -351,7 +334,7 @@ public class BookData implements IDataItem {
     return "";
   }
 
-  public List<SectionData> getVisibleSections(BookScreen.AdvancementCache advancementCache) {
+  public List<SectionData> getVisibleSections(@Nullable BookScreen.AdvancementCache advancementCache) {
     List<SectionData> visible = new ArrayList<>();
 
     for (SectionData section : this.sections) {
@@ -377,6 +360,7 @@ public class BookData implements IDataItem {
     }
   }
 
+  @SuppressWarnings("unused") // API
   public void addRepository(@Nullable BookRepository repository) {
     if (repository != null && !this.repositories.contains(repository)) {
       this.repositories.add(repository);

src/main/java/slimeknights/mantle/client/book/data/BookmarkData.java
@@ -2,10 +2,11 @@ package slimeknights.mantle.client.book.data;
 
 public class BookmarkData {
 
-  public String text = "";
+  public String text;
   public int color = 0x464646; // Index bookmark : 0x1ED400
-  public String page = "";
+  public String page;
 
+  @SuppressWarnings("unused") // used implicitly by GSON
   public BookmarkData() {
     this("");
   }

src/main/java/slimeknights/mantle/client/book/data/LinkedSectionData.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 
 public class LinkedSectionData extends SectionData {
 
-  private transient ArrayList<SectionData> sections = new ArrayList<>();
+  private final transient ArrayList<SectionData> sections = new ArrayList<>();
 
   public LinkedSectionData() {
     super(true);

src/main/java/slimeknights/mantle/client/book/data/PageData.java
@@ -9,6 +9,7 @@ import slimeknights.mantle.client.book.data.content.PageContent;
 import slimeknights.mantle.client.book.data.element.IDataElement;
 import slimeknights.mantle.client.book.repository.BookRepository;
 
+import javax.annotation.Nullable;
 import java.lang.reflect.Array;
 import java.lang.reflect.Field;
 import java.lang.reflect.Modifier;
@@ -25,6 +26,7 @@ public class PageData implements IDataItem {
   public transient BookRepository source;
   public transient PageContent content;
 
+  @SuppressWarnings("unused") // used implicitly by GSON
   public PageData() {
     this(false);
   }
@@ -35,12 +37,12 @@ public class PageData implements IDataItem {
     }
   }
 
+  @SuppressWarnings("unused") // utility
   public String translate(String string) {
     return this.parent.translate(string);
   }
 
   @Override
-  @SuppressWarnings("unchecked")
   public void load() {
     if (this.name == null) {
       this.name = "page" + this.parent.unnamedPageCounter++;
@@ -55,23 +57,31 @@ public class PageData implements IDataItem {
         if (!data.isEmpty()) {
           Class<? extends PageContent> ctype = BookLoader.getPageType(this.type);
 
-          try {
-            this.content = BookLoader.GSON.fromJson(data, ctype);
-          }
-          catch (Exception e) {
-            this.content = new ContentError(ctype == null ? "Failed to create a page of type \"" + this.type + "\", perhaps the type is not registered?" : "Failed to create a page of type \"" + this.type + "\", perhaps the page file \"" + this.data + "\" is missing or invalid?", e);
+          if (ctype != null) {
+            try {
+              this.content = BookLoader.GSON.fromJson(data, ctype);
+            } catch (Exception e) {
+              this.content = new ContentError("Failed to create a page of type \"" + this.type + "\", perhaps the page file \"" + this.data + "\" is missing or invalid?", e);
+            }
+          } else {
+            this.content = new ContentError("Failed to create a page of type \"" + this.type + "\" as it is not registered.");
           }
         }
       }
     }
 
     if (this.content == null) {
-      try {
-        this.content = BookLoader.getPageType(this.type).newInstance();
-      }
-      catch (InstantiationException | IllegalAccessException | NullPointerException e) {
-        this.content = new ContentError("Failed to create a page of type \"" + this.type + "\", perhaps the type is not registered?");
-      }
+        Class<? extends PageContent> ctype = BookLoader.getPageType(this.type);
+
+        if(ctype != null) {
+          try {
+            this.content = ctype.newInstance();
+          } catch (InstantiationException | IllegalAccessException | NullPointerException e) {
+            this.content = new ContentError("Failed to create a page of type \"" + this.type + "\".", e);
+          }
+        } else {
+          this.content = new ContentError("Failed to create a page of type \"" + this.type + "\" as it is not registered.");
+        }
     }
 
     try {
@@ -108,7 +118,7 @@ public class PageData implements IDataItem {
     }
   }
 
-  private void processObject(Object o, final int depth) {
+  private void processObject(@Nullable Object o, final int depth) {
     if(depth > 4 || o == null)
       return;
 

src/main/java/slimeknights/mantle/client/book/data/PageDataModule.java
@@ -1,7 +1,0 @@
-package slimeknights.mantle.client.book.data;
-
-public class PageDataModule extends PageData {
-
-  /** Module required to load this section, see {@link slimeknights.mantle.pulsar.control.PulseManager#isPulseLoaded(String)} for more details */
-  public String module = "";
-}

src/main/java/slimeknights/mantle/client/book/data/content/ContentError.java
@@ -14,7 +14,7 @@ import java.util.ArrayList;
 @OnlyIn(Dist.CLIENT)
 public class ContentError extends PageContent {
 
-  private String errorStage;
+  private final String errorStage;
   private Exception exception;
 
   public ContentError(String errorStage) {

src/main/java/slimeknights/mantle/client/book/data/content/ContentSmelting.java
@@ -3,7 +3,6 @@ package slimeknights.mantle.client.book.data.content;
 import com.google.common.collect.ImmutableList;
 import net.minecraft.block.Blocks;
 import net.minecraft.client.Minecraft;
-import net.minecraft.client.resources.I18n;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
 import net.minecraft.item.crafting.AbstractCookingRecipe;

src/main/java/slimeknights/mantle/client/book/data/element/DataLocation.java
@@ -1,6 +1,7 @@
 package slimeknights.mantle.client.book.data.element;
 
 import net.minecraft.client.renderer.texture.AtlasTexture;
+import net.minecraft.inventory.container.PlayerContainer;
 import net.minecraft.util.ResourceLocation;
 import slimeknights.mantle.client.book.repository.BookRepository;
 
@@ -11,6 +12,6 @@ public class DataLocation implements IDataElement {
 
   @Override
   public void load(BookRepository source) {
-    this.location = "$BLOCK_ATLAS".equals(this.file) ? AtlasTexture.LOCATION_BLOCKS_TEXTURE : source.getResourceLocation(this.file, true);
+    this.location = "$BLOCK_ATLAS".equals(this.file) ? PlayerContainer.LOCATION_BLOCKS_TEXTURE : source.getResourceLocation(this.file, true);
   }
 }

src/main/java/slimeknights/mantle/client/book/data/element/ItemStackData.java
@@ -52,15 +52,15 @@ public class ItemStackData implements IDataElement {
     Item item;
     boolean isMissingItem = false;
     try {
-      item = ForgeRegistries.ITEMS.getValue(new ResourceLocation(this.id));//ItemArgument.getItem(null, id).getItem();
+      item = ForgeRegistries.ITEMS.getValue(new ResourceLocation(this.id));
     }
     catch (Exception e) {
-      item = Item.getItemFromBlock(Blocks.BARRIER);
+      item = Items.BARRIER;
       isMissingItem = true;
     }
 
     if (item == Items.AIR) {
-      item = Item.getItemFromBlock(Blocks.BARRIER);
+      item = Items.BARRIER;
       isMissingItem = true;
     }
 

src/main/java/slimeknights/mantle/client/book/repository/BookRepository.java
@@ -7,29 +7,40 @@ import java.util.List;
 
 import slimeknights.mantle.client.book.data.SectionData;
 
+import javax.annotation.Nullable;
+
 public abstract class BookRepository {
 
+  @SuppressWarnings("StaticInitializerReferencesSubClass") // will only occur in very specific threaded environment
   public static final BookRepository DUMMY = new DummyRepository();
 
   public abstract List<SectionData> getSections();
 
-  public ResourceLocation getResourceLocation(String path) {
+  @Nullable
+  public ResourceLocation getResourceLocation(@Nullable String path) {
     return this.getResourceLocation(path, false);
   }
 
-  public abstract ResourceLocation getResourceLocation(String path, boolean safe);
+  @Nullable
+  public abstract ResourceLocation getResourceLocation(@Nullable String path, boolean safe);
 
-  public abstract IResource getResource(ResourceLocation loc);
+  @Nullable
+  public abstract IResource getResource(@Nullable ResourceLocation loc);
 
-  public boolean resourceExists(String location) {
+  @SuppressWarnings("unused") // API
+  public boolean resourceExists(@Nullable String location) {
+    if(location == null) {
+      return false;
+    }
+
     return this.resourceExists(new ResourceLocation(location));
   }
 
-  public abstract boolean resourceExists(ResourceLocation location);
+  public abstract boolean resourceExists(@Nullable ResourceLocation location);
 
-  public String resourceToString(IResource resource) {
+  public String resourceToString(@Nullable IResource resource) {
     return this.resourceToString(resource, true);
   }
 
-  public abstract String resourceToString(IResource resource, boolean skipCommments);
+  public abstract String resourceToString(@Nullable IResource resource, boolean skipCommments);
 }

src/main/java/slimeknights/mantle/client/book/repository/DummyRepository.java
@@ -4,6 +4,7 @@ import net.minecraft.resources.IResource;
 import net.minecraft.util.ResourceLocation;
 import slimeknights.mantle.client.book.data.SectionData;
 
+import javax.annotation.Nullable;
 import java.util.Collections;
 import java.util.List;
 
@@ -15,22 +16,22 @@ public class DummyRepository extends BookRepository {
   }
 
   @Override
-  public ResourceLocation getResourceLocation(String path, boolean safe) {
-    throw new UnsupportedOperationException();
+  public ResourceLocation getResourceLocation(@Nullable String path, boolean safe) {
+    return null;
   }
 
   @Override
-  public IResource getResource(ResourceLocation loc) {
-    throw new UnsupportedOperationException();
+  public IResource getResource(@Nullable ResourceLocation loc) {
+    return null;
   }
 
   @Override
-  public boolean resourceExists(ResourceLocation location) {
+  public boolean resourceExists(@Nullable ResourceLocation location) {
     return false;
   }
 
   @Override
-  public String resourceToString(IResource resource, boolean skipCommments) {
+  public String resourceToString(@Nullable IResource resource, boolean skipCommments) {
     return "";
   }
 }

src/main/java/slimeknights/mantle/client/book/repository/FileRepository.java
@@ -3,12 +3,13 @@ package slimeknights.mantle.client.book.repository;
 import net.minecraft.client.Minecraft;
 import net.minecraft.resources.IResource;
 import net.minecraft.util.ResourceLocation;
-import org.apache.commons.io.Charsets;
 import org.apache.commons.io.IOUtils;
 import slimeknights.mantle.client.book.BookLoader;
 import slimeknights.mantle.client.book.data.SectionData;
 
+import javax.annotation.Nullable;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Iterator;
@@ -28,13 +29,14 @@ public class FileRepository extends BookRepository {
   }
 
   @Override
-  public ResourceLocation getResourceLocation(String path, boolean safe) {
+  public ResourceLocation getResourceLocation(@Nullable String path, boolean safe) {
     if (path == null) {
       return safe ? new ResourceLocation("") : null;
     }
     if (!path.contains(":")) {
       String langPath = null;
 
+      //noinspection ConstantConditions - this was proven to be null once
       if (Minecraft.getInstance().getLanguageManager() != null && Minecraft.getInstance().getLanguageManager().getCurrentLanguage() != null) {
         langPath = Minecraft.getInstance().getLanguageManager().getCurrentLanguage().getCode();
       }
@@ -43,6 +45,7 @@ public class FileRepository extends BookRepository {
 
       ResourceLocation res;
 
+      //noinspection ConstantConditions - see above
       if (langPath != null) {
         res = new ResourceLocation(this.location + "/" + langPath + "/" + path);
         if (this.resourceExists(res)) {
@@ -57,19 +60,19 @@ public class FileRepository extends BookRepository {
       if (this.resourceExists(res)) {
         return res;
       }
-      return safe ? new ResourceLocation("") : null;
     }
     else {
       ResourceLocation res = new ResourceLocation(path);
       if (this.resourceExists(res)) {
         return res;
       }
-      return safe ? new ResourceLocation("") : null;
     }
+
+    return safe ? new ResourceLocation("") : null;
   }
 
   @Override
-  public IResource getResource(ResourceLocation loc) {
+  public IResource getResource(@Nullable ResourceLocation loc) {
     if (loc == null) {
       return null;
     }
@@ -82,7 +85,7 @@ public class FileRepository extends BookRepository {
   }
 
   @Override
-  public boolean resourceExists(ResourceLocation location) {
+  public boolean resourceExists(@Nullable ResourceLocation location) {
     if (location == null) {
       return false;
     }
@@ -96,18 +99,18 @@ public class FileRepository extends BookRepository {
   }
 
   @Override
-  public String resourceToString(IResource resource, boolean skipCommments) {
+  public String resourceToString(@Nullable IResource resource, boolean skipCommments) {
     if (resource == null) {
       return "";
     }
     try {
-      Iterator iterator = IOUtils.readLines(resource.getInputStream(), Charsets.UTF_8).iterator();
+      Iterator<String> iterator = IOUtils.readLines(resource.getInputStream(), StandardCharsets.UTF_8).iterator();
       StringBuilder builder = new StringBuilder();
 
       boolean isLongComment = false;
 
       while (iterator.hasNext()) {
-        String s = ((String) iterator.next()).trim() + "\n";
+        String s = iterator.next().trim() + "\n";
 
         // Comment skipper
         if (skipCommments) {

src/main/java/slimeknights/mantle/network/MantleNetwork.java
@@ -2,7 +2,6 @@ package slimeknights.mantle.network;
 
 import net.minecraftforge.fml.network.NetworkDirection;
 import slimeknights.mantle.Mantle;
-import slimeknights.mantle.network.packet.FluidUpdatePacket;
 import slimeknights.mantle.network.packet.UpdateSavedPagePacket;
 
 public class MantleNetwork {