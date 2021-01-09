63b013b9b4ef69465e580bdac947269195defe50
David Burnett <knightminer4@gmail.com>
2021-01-08 22:16:09 -0500

    Add recipe serializer base class
    Because its annoying for every serializer to extend one thing and implement another

---
+++ src/main/java/slimeknights/mantle/recipe/RecipeSerializer.java
@@ -0,0 +1,11 @@
+package slimeknights.mantle.recipe;
+
+import net.minecraft.item.crafting.IRecipe;
+import net.minecraft.item.crafting.IRecipeSerializer;
+import net.minecraftforge.registries.ForgeRegistryEntry;
+
+/**
+ * This class simply exists because every recipe serializer has to extend the forge registry entry and implement the interface. Easier to just extend this class
+ * @param <T> Recipe type
+ */
+public abstract class RecipeSerializer<T extends IRecipe<?>> extends ForgeRegistryEntry<IRecipeSerializer<?>> implements IRecipeSerializer<T> {}
