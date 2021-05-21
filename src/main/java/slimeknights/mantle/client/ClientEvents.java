e77198e67b5f7bcab3f9f1ba348779bd05fbec97
KnightMiner <knightminer4@gmail.com>
2021-05-21 01:08:01 -0400

    Add new fluid texture model
    Allows resource packs to control fluid textures, provide the fluid uses TexturedFluidAttributes

--- src/main/java/slimeknights/mantle/client/ClientEvents.java
+++ src/main/java/slimeknights/mantle/client/ClientEvents.java
@@ -17,6 +17,7 @@ import slimeknights.mantle.client.book.BookLoader;
 import slimeknights.mantle.client.model.FallbackModelLoader;
 import slimeknights.mantle.client.model.RetexturedModel;
 import slimeknights.mantle.client.model.connected.ConnectedModel;
+import slimeknights.mantle.client.model.fluid.FluidTextureModel;
 import slimeknights.mantle.client.model.fluid.FluidsModel;
 import slimeknights.mantle.client.model.inventory.InventoryModel;
 import slimeknights.mantle.client.model.util.ModelHelper;
@@ -40,6 +41,7 @@ public class ClientEvents {
     ModelLoaderRegistry.registerLoader(Mantle.getResource("connected"), ConnectedModel.Loader.INSTANCE);
     ModelLoaderRegistry.registerLoader(Mantle.getResource("fluids"), FluidsModel.Loader.INSTANCE);
     ModelLoaderRegistry.registerLoader(Mantle.getResource("retextured"), RetexturedModel.Loader.INSTANCE);
+    ModelLoaderRegistry.registerLoader(Mantle.getResource("fluid_texture"), FluidTextureModel.LOADER);
   }
 
   @SubscribeEvent
