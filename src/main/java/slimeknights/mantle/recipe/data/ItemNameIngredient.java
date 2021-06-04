6778156cc0a15248e126c7124945cc18236860a7
KnightMiner <knightminer4@gmail.com>
2021-06-04 17:59:01 -0400

    Add some extra ingredients and objects for datagen
    CompoundIngredient and NBTIngredient are simply forge ingredients with public constructors
    ItemNameIngredient and NBTNameIngredient allow referencing items from other mods for inputs, for compat recipes
    ItemNameOutput works with ItemOutput for referencing items from another mod in output

---
+++ src/main/java/slimeknights/mantle/recipe/data/ItemNameIngredient.java
@@ -0,0 +1,66 @@
+package slimeknights.mantle.recipe.data;
+
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.crafting.Ingredient;
+import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.common.crafting.IIngredientSerializer;
+import net.minecraftforge.common.crafting.VanillaIngredientSerializer;
+
+import javax.annotation.Nullable;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
+/**
+ * Ingredient for a non-NBT sensitive item from another mod, should never be used outside datagen
+ */
+public class ItemNameIngredient extends Ingredient {
+  private final List<ResourceLocation> names;
+
+  protected ItemNameIngredient(List<ResourceLocation> names) {
+    super(Stream.empty());
+    this.names = names;
+  }
+
+  /** Creates a new ingredient from a list of names */
+  public static ItemNameIngredient from(List<ResourceLocation> names) {
+    return new ItemNameIngredient(names);
+  }
+
+  /** Creates a new ingredient from a list of names */
+  public static ItemNameIngredient from(ResourceLocation... names) {
+    return from(Arrays.asList(names));
+  }
+
+  @Override
+  public boolean test(@Nullable ItemStack stack) {
+    throw new UnsupportedOperationException();
+  }
+
+  /** Creates a JSON object for a name */
+  private static JsonObject forName(ResourceLocation name) {
+    JsonObject json = new JsonObject();
+    json.addProperty("item", name.toString());
+    return json;
+  }
+
+  @Override
+  public JsonElement serialize() {
+    if (names.size() == 1) {
+      return forName(names.get(0));
+    }
+    JsonArray array = new JsonArray();
+    for (ResourceLocation name : names) {
+      array.add(forName(name));
+    }
+    return array;
+  }
+
+  @Override
+  public IIngredientSerializer<? extends Ingredient> getSerializer() {
+    return VanillaIngredientSerializer.INSTANCE;
+  }
+}
---
+++ src/main/java/slimeknights/mantle/recipe/data/NBTIngredient.java
@@ -0,0 +1,16 @@
+package slimeknights.mantle.recipe.data;
+
+import net.minecraft.item.ItemStack;
+
+/**
+ * Simply an extension of the Forge ingredient because the constructor is not public for some dumb reason
+ */
+public class NBTIngredient extends net.minecraftforge.common.crafting.NBTIngredient {
+  protected NBTIngredient(ItemStack stack) {
+    super(stack);
+  }
+
+  public static NBTIngredient from(ItemStack stack) {
+    return new NBTIngredient(stack);
+  }
+}
