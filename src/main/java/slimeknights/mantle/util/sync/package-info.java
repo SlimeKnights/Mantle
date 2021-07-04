a13d1046d2ad1a5da6dba2acb349eeedefb903a7
KnightMiner <knightminer4@gmail.com>
2021-07-03 20:58:41 -0400

    Add a few utilities from Tinkers
    Includes helpers for container sync and the single key multimap

---
+++ src/main/java/slimeknights/mantle/util/sync/package-info.java
@@ -0,0 +1,7 @@
+@ParametersAreNonnullByDefault
+@MethodsReturnNonnullByDefault
+package slimeknights.mantle.util.sync;
+
+import mcp.MethodsReturnNonnullByDefault;
+
+import javax.annotation.ParametersAreNonnullByDefault;
