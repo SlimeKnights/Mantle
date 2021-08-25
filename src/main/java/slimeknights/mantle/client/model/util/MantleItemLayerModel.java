608e015c74cd81af394020ca19062720c1cb7933
KnightMiner <knightminer4@gmail.com>
2021-08-25 14:12:03 -0400

    Move mantle item layer model to utils
    More consistent with simple block model as its primarily for use in other models

--- src/main/java/slimeknights/mantle/client/model/util/MantleItemLayerModel.java
+++ src/main/java/slimeknights/mantle/client/model/util/MantleItemLayerModel.java
@@ -1,4 +1,4 @@
-package slimeknights.mantle.client.model;
+package slimeknights.mantle.client.model.util;
 
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
