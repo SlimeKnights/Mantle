fb2adabae2cdd602c25938b5373a944677fed185
KnightMiner <knightminer4@gmail.com>
2021-12-24 20:14:04 -0500

    Bring back offhand cooldown indicator

--- src/main/java/slimeknights/mantle/client/ClientEvents.java
+++ src/main/java/slimeknights/mantle/client/ClientEvents.java
@@ -1,12 +1,22 @@
 package slimeknights.mantle.client;
 
+import com.mojang.blaze3d.platform.GlStateManager;
+import com.mojang.blaze3d.systems.RenderSystem;
+import com.mojang.blaze3d.vertex.PoseStack;
+import net.minecraft.client.AttackIndicatorStatus;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.Options;
+import net.minecraft.client.gui.GuiComponent;
 import net.minecraft.client.renderer.Sheets;
 import net.minecraft.server.packs.resources.ReloadableResourceManager;
 import net.minecraft.server.packs.resources.ResourceManager;
+import net.minecraft.world.entity.HumanoidArm;
+import net.minecraft.world.level.GameType;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.client.event.ModelRegistryEvent;
 import net.minecraftforge.client.event.RenderGameOverlayEvent;
+import net.minecraftforge.client.gui.ForgeIngameGui;
+import net.minecraftforge.client.gui.IIngameOverlay;
 import net.minecraftforge.client.model.ModelLoaderRegistry;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.EventPriority;
@@ -71,17 +81,19 @@ public class ClientEvents {
       ((ReloadableResourceManager)manager).registerReloadListener(new BookLoader());
     }
     MinecraftForge.EVENT_BUS.register(new ExtraHeartRenderHandler());
-    MinecraftForge.EVENT_BUS.addListener(EventPriority.NORMAL, false, RenderGameOverlayEvent.Post.class, ClientEvents::renderOffhandAttackIndicator);
+    MinecraftForge.EVENT_BUS.addListener(EventPriority.NORMAL, false, RenderGameOverlayEvent.PostLayer.class, ClientEvents::renderOffhandAttackIndicator);
   }
 
   // registered with FORGE bus
-  private static void renderOffhandAttackIndicator(RenderGameOverlayEvent.Post event) {
-    // TODO: update
-    /*
+  private static void renderOffhandAttackIndicator(RenderGameOverlayEvent.PostLayer event) {
     // must have a player, not be in spectator, and have the indicator enabled
     Minecraft minecraft = Minecraft.getInstance();
     Options settings = minecraft.options;
     if (minecraft.player == null || minecraft.gameMode == null || minecraft.gameMode.getPlayerMode() == GameType.SPECTATOR || settings.attackIndicator == AttackIndicatorStatus.OFF) {
+      return;
+    }
+    IIngameOverlay overlay = event.getOverlay();
+    if (overlay != ForgeIngameGui.CROSSHAIR_ELEMENT && overlay != ForgeIngameGui.HOTBAR_ELEMENT) {
       return;
     }
 
@@ -95,7 +107,7 @@ public class ClientEvents {
     PoseStack matrixStack = event.getMatrixStack();
     switch (settings.attackIndicator) {
       case CROSSHAIR:
-        if (event.getType() == ElementType.CROSSHAIRS && minecraft.options.getCameraType().isFirstPerson()) {
+        if (overlay == ForgeIngameGui.CROSSHAIR_ELEMENT && minecraft.options.getCameraType().isFirstPerson()) {
           if (!settings.renderDebug || settings.hideGui || minecraft.player.isReducedDebugInfo() || settings.reducedDebugInfo) {
             // mostly cloned from vanilla attack indicator
             RenderSystem.blendFuncSeparate(GlStateManager.SourceFactor.ONE_MINUS_DST_COLOR, GlStateManager.DestFactor.ONE_MINUS_SRC_COLOR, GlStateManager.SourceFactor.ONE, GlStateManager.DestFactor.ZERO);
@@ -104,14 +116,14 @@ public class ClientEvents {
             int y = (scaledHeight / 2) - 14 + (2 * (scaledHeight % 2));
             int x = minecraft.getWindow().getGuiScaledWidth() / 2 - 8;
             int width = (int)(cooldown * 17.0F);
-            minecraft.getTextureManager().bindForSetup(GuiComponent.GUI_ICONS_LOCATION);
+            RenderSystem.setShaderTexture(0, GuiComponent.GUI_ICONS_LOCATION);
             minecraft.gui.blit(matrixStack, x, y, 36, 94, 16, 4);
             minecraft.gui.blit(matrixStack, x, y, 52, 94, width, 4);
           }
         }
         break;
       case HOTBAR:
-        if (event.getType() == ElementType.HOTBAR && minecraft.cameraEntity == minecraft.player) {
+        if (overlay == ForgeIngameGui.HOTBAR_ELEMENT && minecraft.cameraEntity == minecraft.player) {
           int centerWidth = minecraft.getWindow().getGuiScaledWidth() / 2;
           int y = minecraft.getWindow().getGuiScaledHeight() - 20;
           int x;
@@ -121,7 +133,7 @@ public class ClientEvents {
           } else {
             x = centerWidth + 91 + 6 + 32;
           }
-          minecraft.getTextureManager().bindForSetup(GuiComponent.GUI_ICONS_LOCATION);
+          RenderSystem.setShaderTexture(0, GuiComponent.GUI_ICONS_LOCATION);
           int l1 = (int)(cooldown * 19.0F);
           RenderSystem.setShaderColor(1.0F, 1.0F, 1.0F, 1.0F);
           minecraft.gui.blit(matrixStack, x, y, 0, 94, 18, 18);
@@ -129,6 +141,5 @@ public class ClientEvents {
         }
         break;
     }
-     */
   }
 }
