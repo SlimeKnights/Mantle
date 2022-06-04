526c5ffbaa57fae667f79dbe292a0096827dd668
KnightMiner <knightminer4@gmail.com>
2022-06-04 18:30:08 -0400

    Port over many JSON utils from tinkers

--- src/main/java/slimeknights/mantle/util/JsonHelper.java
+++ src/main/java/slimeknights/mantle/util/JsonHelper.java
@@ -4,22 +4,42 @@ import com.google.common.collect.ImmutableList;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
+import com.google.gson.JsonParseException;
 import com.google.gson.JsonSyntaxException;
-import lombok.AccessLevel;
-import lombok.NoArgsConstructor;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.server.packs.resources.Resource;
+import net.minecraft.server.packs.resources.ResourceManager;
 import net.minecraft.util.GsonHelper;
+import net.minecraftforge.event.OnDatapackSyncEvent;
+import net.minecraftforge.network.PacketDistributor;
+import net.minecraftforge.network.PacketDistributor.PacketTarget;
+import net.minecraftforge.registries.IForgeRegistry;
+import net.minecraftforge.registries.IForgeRegistryEntry;
+import slimeknights.mantle.Mantle;
+import slimeknights.mantle.network.NetworkWrapper;
+import slimeknights.mantle.network.packet.ISimplePacket;
 
 import javax.annotation.Nullable;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
 import java.util.List;
+import java.util.Locale;
+import java.util.Objects;
 import java.util.function.BiFunction;
 import java.util.function.Function;
+import java.util.stream.Stream;
 
 /**
  * Utilities to help in parsing JSON
  */
-@NoArgsConstructor(access = AccessLevel.PRIVATE)
+@SuppressWarnings("unused")
 public class JsonHelper {
+  private JsonHelper() {}
+
   /**
    * Gets an element from JSON, throwing an exception if missing
    * @param json        Object parent
@@ -122,6 +142,61 @@ public class JsonHelper {
   }
 
   /**
+   * Parses a registry entry from JSON
+   * @param registry  Registry
+   * @param element   Element to deserialize
+   * @param key       Json key
+   * @param <T>  Object type
+   * @return  Registry value
+   * @throws JsonSyntaxException  If something failed to parse
+   */
+  public static <T extends IForgeRegistryEntry<T>> T convertToEntry(IForgeRegistry<T> registry, JsonElement element, String key) {
+    ResourceLocation name = JsonHelper.convertToResourceLocation(element, key);
+    if (registry.containsKey(name)) {
+      T value = registry.getValue(name);
+      if (value != null) {
+        return value;
+      }
+    }
+    throw new JsonSyntaxException("Unknown " + registry.getRegistryName() + " " + name);
+  }
+
+  /**
+   * Parses a registry entry from JSON
+   * @param registry  Registry
+   * @param parent    Parent JSON object
+   * @param key       Json key
+   * @param <T>  Object type
+   * @return  Registry value
+   * @throws JsonSyntaxException  If something failed to parse
+   */
+  public static <T extends IForgeRegistryEntry<T>> T getAsEntry(IForgeRegistry<T> registry, JsonObject parent, String key) {
+    return convertToEntry(registry, JsonHelper.getElement(parent, key), key);
+  }
+
+  /** Parses an enum from its name */
+  private static <T extends Enum<T>> T enumByName(String name, Class<T> enumClass) {
+    for (T value : enumClass.getEnumConstants()) {
+      if (value.name().toLowerCase(Locale.ROOT).equals(name)) {
+        return value;
+      }
+    }
+    throw new JsonSyntaxException("Invalid " + enumClass.getSimpleName() + " " + name);
+  }
+
+  /** Gets an enum value from its string name */
+  public static <T extends Enum<T>> T convertToEnum(JsonElement element, String key, Class<T> enumClass) {
+    String name = GsonHelper.convertToString(element, key);
+    return enumByName(name, enumClass);
+  }
+
+  /** Gets an enum value from its string name */
+  public static <T extends Enum<T>> T getAsEnum(JsonObject json, String key, Class<T> enumClass) {
+    String name = GsonHelper.getAsString(json, key);
+    return enumByName(name, enumClass);
+  }
+
+  /**
    * Parses a color as a string
    * @param color  Color to parse
    * @return  Parsed string
@@ -146,5 +221,73 @@ public class JsonHelper {
       }
     }
     throw new JsonSyntaxException("Invalid color '" + color + "'");
+  }
+
+
+  /* Resource loaders */
+
+  /**
+   * Converts the resource into a JSON file
+   * @param resource  Resource to read. Closed when done
+   * @return  JSON object, or null if failed to parse
+   */
+  @Nullable
+  public static JsonObject getJson(Resource resource) {
+    try (BufferedReader reader = new BufferedReader(new InputStreamReader(resource.getInputStream(), StandardCharsets.UTF_8))) {
+      return GsonHelper.parse(reader);
+    } catch (JsonParseException | IOException e) {
+      Mantle.logger.error("Failed to load JSON from resource " + resource.getLocation(), e);
+      return null;
+    }
+  }
+
+  /** Gets a list of JSON objects for a single path in all domains and packs, for a language file like loader */
+  public static List<JsonObject> getFileInAllDomainsAndPacks(ResourceManager manager, String path) {
+    return manager
+      .getNamespaces().stream()
+      .filter(ResourceLocation::isValidNamespace)
+      .flatMap(namespace -> {
+        ResourceLocation location = new ResourceLocation(namespace, path);
+        try {
+          return manager.getResources(location).stream();
+        } catch (FileNotFoundException e) {
+          // suppress, the above method throws instead of returning empty
+        } catch (IOException e) {
+          Mantle.logger.error("Failed to load JSON files from {}", location, e);
+        }
+        return Stream.empty();
+      })
+      .map(JsonHelper::getJson)
+      .filter(Objects::nonNull).toList();
+  }
+
+  /** Sends the packet to the given player */
+  private static void sendPackets(NetworkWrapper network, ServerPlayer player, ISimplePacket[] packets) {
+    // on an integrated server, the modifier registries have a single instance on both the client and the server thread
+    // this means syncing is unneeded, and has the side-effect of recreating all the modifier instances (which can lead to unexpected behavior)
+    // as a result, integrated servers just mark fullyLoaded as true without syncing anything, side-effect is listeners may run twice on single player
+
+    // on a dedicated server, the client is running a separate game instance, this is where we send packets, plus fully loaded should already be true
+    // this event is not fired when connecting to a server
+    if (!player.connection.getConnection().isMemoryConnection()) {
+      PacketTarget target = PacketDistributor.PLAYER.with(() -> player);
+      for (ISimplePacket packet : packets) {
+        network.send(target, packet);
+      }
+    }
+  }
+
+  /** Called when the player logs in to send packets */
+  public static void syncPackets(OnDatapackSyncEvent event, NetworkWrapper network, ISimplePacket... packets) {
+    // send to single player
+    ServerPlayer targetedPlayer = event.getPlayer();
+    if (targetedPlayer != null) {
+      sendPackets(network, targetedPlayer, packets);
+    } else {
+      // send to all players
+      for (ServerPlayer player : event.getPlayerList().getPlayers()) {
+        sendPackets(network, player, packets);
+      }
+    }
   }
 }
