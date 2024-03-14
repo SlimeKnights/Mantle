527e83140e9b396fd1c395ab83b81c8779ea64b3
KnightMiner <knightminer4@gmail.com>
2024-03-14 14:34:58 -0400

    Bring back the generic loot modifier builder
    Misremembered its purpose

--- src/main/java/slimeknights/mantle/loot/AbstractLootModifierBuilder.java
+++ src/main/java/slimeknights/mantle/loot/AbstractLootModifierBuilder.java
@@ -1,10 +1,13 @@
 package slimeknights.mantle.loot;
 
+import lombok.RequiredArgsConstructor;
 import net.minecraft.world.level.storage.loot.predicates.LootItemCondition;
 import net.minecraftforge.common.data.GlobalLootModifierProvider;
+import net.minecraftforge.common.loot.LootModifier;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Function;
 
 /** Base builder for a global loot modifier during datagen, intended to be used with {@link GlobalLootModifierProvider} */
 public abstract class AbstractLootModifierBuilder<B extends AbstractLootModifierBuilder<B>> {
@@ -20,5 +23,16 @@ public abstract class AbstractLootModifierBuilder<B extends AbstractLootModifier
   /** Gets the built list of conditions */
   protected LootItemCondition[] getConditions() {
     return conditions.toArray(new LootItemCondition[0]);
+  }
+
+  /** Generic builder for a modifier that just takes conditions */
+  @RequiredArgsConstructor
+  public static class GenericLootModifierBuilder<M extends LootModifier> extends AbstractLootModifierBuilder<GenericLootModifierBuilder<M>> {
+    private final Function<LootItemCondition[],M> constructor;
+
+    /** Builds the final instance */
+    public M build() {
+      return constructor.apply(getConditions());
+    }
   }
 }
