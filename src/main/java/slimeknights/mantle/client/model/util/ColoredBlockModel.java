a4d6fde0558d8b3d282dd53893bca2073c1bf9a0
KnightMiner <knightminer4@gmail.com>
2024-03-14 15:47:53 -0400

    Rework colored block model's face baking
    Coloring and luminosity are now handled via a quad transformer, which means we no longer need to duplicate most of face bakery
    Luminsity specifically is now deprecated in favor of forge's emissivity field.

--- src/main/java/slimeknights/mantle/client/model/util/ColoredBlockModel.java
+++ src/main/java/slimeknights/mantle/client/model/util/ColoredBlockModel.java
@@ -6,7 +6,6 @@ import com.mojang.datafixers.util.Either;
 import com.mojang.math.Transformation;
 import com.mojang.math.Vector3f;
 import lombok.Getter;
-import net.minecraft.client.renderer.FaceInfo;
 import net.minecraft.client.renderer.block.model.BakedQuad;
 import net.minecraft.client.renderer.block.model.BlockElement;
 import net.minecraft.client.renderer.block.model.BlockElementFace;
@@ -27,8 +26,10 @@ import net.minecraft.util.GsonHelper;
 import net.minecraft.util.Mth;
 import net.minecraftforge.client.ForgeHooksClient;
 import net.minecraftforge.client.model.IQuadTransformer;
+import net.minecraftforge.client.model.QuadTransformers;
 import net.minecraftforge.client.model.geometry.IGeometryBakingContext;
 import net.minecraftforge.client.model.geometry.IGeometryLoader;
+import slimeknights.mantle.Mantle;
 import slimeknights.mantle.util.JsonHelper;
 import slimeknights.mantle.util.LogicHelper;
 
@@ -43,6 +44,7 @@ import static net.minecraft.client.renderer.block.model.BlockModel.FACE_BAKERY;
 /**
  * Block model for setting color, luminosity, and per element uv lock. Similar to {@link MantleItemLayerModel} but for blocks
  */
+@SuppressWarnings("unused")  // API
 public class ColoredBlockModel extends SimpleBlockModel {
   /** Model loader to allow doing basic coloring outside of other models */
   public static final IGeometryLoader<SimpleBlockModel> LOADER = ColoredBlockModel::deserialize;
@@ -73,15 +75,14 @@ public class ColoredBlockModel extends SimpleBlockModel {
    * @param builder          Baked model builder
    * @param owner            Model owner
    * @param part             Part to bake
-   * @param color            Color tint, use -1 for no tint
-   * @param luminosity       Luminosity for fullbright, use 0 for normal lighting
+   * @param emissivity       Emissivity for fullbright, -1 will leave forge in charge, 0-15 will override the forge value
    * @param spriteGetter     Sprite getter
    * @param transform        Transform for the face
-   * @param quadTransformer  Forge transformations for the face
+   * @param quadTransformer  Forge transformations for the face, this is notably where you should handle color transformations
    * @param uvlock           UV lock for the face, separated to allow overriding the model state
    * @param location         Model location
    */
-  public static void bakePart(Builder builder, IGeometryBakingContext owner, BlockElement part, int color, int luminosity, Function<Material,TextureAtlasSprite> spriteGetter, Transformation transform, IQuadTransformer quadTransformer, boolean uvlock, ResourceLocation location) {
+  public static void bakePart(Builder builder, IGeometryBakingContext owner, BlockElement part, int emissivity, Function<Material,TextureAtlasSprite> spriteGetter, Transformation transform, IQuadTransformer quadTransformer, boolean uvlock, ResourceLocation location) {
     for (Direction direction : part.faces.keySet()) {
       BlockElementFace face = part.faces.get(direction);
       // ensure the name is not prefixed (it always is)
@@ -91,7 +92,7 @@ public class ColoredBlockModel extends SimpleBlockModel {
       }
       // bake the face with the extra colors
       TextureAtlasSprite sprite = spriteGetter.apply(owner.getMaterial(texture));
-      BakedQuad quad = bakeFace(part, face, sprite, direction, transform, uvlock, color, luminosity, location);
+      BakedQuad quad = bakeFace(part, face, sprite, direction, transform, uvlock, emissivity, location);
       quadTransformer.processInPlace(quad);
       // apply cull face
       //noinspection ConstantConditions  the annotation is a liar
@@ -124,7 +125,11 @@ public class ColoredBlockModel extends SimpleBlockModel {
     for (int i = 0; i < size; i++) {
       BlockElement part = elements.get(i);
       ColorData colors = LogicHelper.getOrDefault(colorData, i, ColorData.DEFAULT);
-      bakePart(builder, owner, part, colors.color, colors.luminosity(), spriteGetter, transformation, quadTransformer, colors.isUvLock(uvlock), location);
+      if (colors.luminosity != -1 && !location.equals(BAKE_LOCATION)) {
+        Mantle.logger.warn("Using deprecated 'luminosity' field on ColoredBlockModel color data for {}, this will be removed in 1.20 in favor of Forge's 'emissivity'.", location);
+      }
+      IQuadTransformer partTransformer = colors.color == -1 ? quadTransformer : quadTransformer.andThen(applyColorQuadTransformer(colors.color));
+      bakePart(builder, owner, part, colors.luminosity, spriteGetter, transformation, partTransformer, colors.isUvLock(uvlock), location);
     }
     return builder.build(getRenderTypeGroup(owner));
   }
@@ -142,8 +147,8 @@ public class ColoredBlockModel extends SimpleBlockModel {
   /**
    * Data class for setting properties when baking colored elements
    */
-  public record ColorData(int color, int luminosity, @Nullable Boolean uvlock) {
-    public static final ColorData DEFAULT = new ColorData(-1, 0, null);
+  public record ColorData(int color, @Deprecated int luminosity, @Nullable Boolean uvlock) {
+    public static final ColorData DEFAULT = new ColorData(-1, -1, null);
 
     /** Gets the UV lock for the given part */
     public boolean isUvLock(boolean defaultLock) {
@@ -158,7 +163,7 @@ public class ColoredBlockModel extends SimpleBlockModel {
      */
     public static ColorData fromJson(JsonObject json) {
       int color = JsonHelper.parseColor(GsonHelper.getAsString(json, "color", ""));
-      int luminosity = GsonHelper.getAsInt(json, "luminosity", 0);
+      int luminosity = GsonHelper.getAsInt(json, "luminosity", -1);
       Boolean uvlock = null;
       if (json.has("uvlock")) {
         uvlock = GsonHelper.getAsBoolean(json, "uvlock");
@@ -181,23 +186,45 @@ public class ColoredBlockModel extends SimpleBlockModel {
   /* Face bakery */
 
   /**
-   * Extension of {@code BlockModel#bakeFace(BlockPart, BlockPartFace, TextureAtlasSprite, Direction, IModelTransform, ResourceLocation)} with color and luminosity arguments
+   * Converts an ARGB color to an ABGR color, as the commonly used color format is not the format colors end up packed into.
+   * This function doubles as its own inverse, not that its needed.
+   * @param color  ARGB color
+   * @return  ABGR color
+   */
+  public static int swapColorRedBlue(int color) {
+    return (color & 0xFF00FF00) // alpha and green same spot
+           | ((color >> 16) & 0x000000FF) // red moves to blue
+           | ((color << 16) & 0x00FF0000); // blue moves to red
+  }
+
+  /** Quad transformer applying a static color */
+  public static IQuadTransformer applyColorQuadTransformer(int color) {
+    int abgr = swapColorRedBlue(color);
+    return quad -> {
+      int[] vertices = quad.getVertices();
+      for (int i = 0; i < 4; i++) {
+        vertices[i * IQuadTransformer.STRIDE + IQuadTransformer.COLOR] = abgr;
+      }
+    };
+  }
+
+  /**
+   * Extension of {@code BlockModel#bakeFace(BlockPart, BlockPartFace, TextureAtlasSprite, Direction, IModelTransform, ResourceLocation)} with emissivity and UV lock arguments
    * @param part        Part containing the face
    * @param face        Face data
    * @param sprite      Sprite for the face
    * @param facing      Direction of the face
    * @param transform   Transform for the face
    * @param uvlock      UV lock for the face, separated to allow overriding the model state
-   * @param color       Hard tint for the part in AARRGGBB format, use -1 for no tint
-   * @param luminosity  Lighting for the part, 0 for no extra lighting
+   * @param emissivity  Emissivity for fullbright, -1 will leave forge in charge, 0-15 will override the forge value
    * @param location    Model location for errors
    */
-  public static BakedQuad bakeFace(BlockElement part, BlockElementFace face, TextureAtlasSprite sprite, Direction facing, Transformation transform, boolean uvlock, int color, int luminosity, ResourceLocation location) {
-    return bakeQuad(part.from, part.to, face, sprite, facing, transform, uvlock, part.rotation, part.shade, color, luminosity, location);
+  public static BakedQuad bakeFace(BlockElement part, BlockElementFace face, TextureAtlasSprite sprite, Direction facing, Transformation transform, boolean uvlock, int emissivity, ResourceLocation location) {
+    return bakeQuad(part.from, part.to, face, sprite, facing, transform, uvlock, part.rotation, part.shade, emissivity, location);
   }
 
   /**
-   * Extension of {@link FaceBakery#bakeQuad(Vector3f, Vector3f, BlockElementFace, TextureAtlasSprite, Direction, ModelState, BlockElementRotation, boolean, ResourceLocation)} with color and luminosity arguments
+   * Extension of {@link FaceBakery#bakeQuad(Vector3f, Vector3f, BlockElementFace, TextureAtlasSprite, Direction, ModelState, BlockElementRotation, boolean, ResourceLocation)} with emissivity and UV lock overrides
    * @param posFrom        Face start position
    * @param posTo          Face end position
    * @param face           Face data
@@ -207,14 +234,13 @@ public class ColoredBlockModel extends SimpleBlockModel {
    * @param uvlock         UV lock for the face, separated to allow overriding the model state
    * @param partRotation   Rotation for the part
    * @param shade          If true, shades the part
-   * @param color          Hard tint for the part in AARRGGBB format, use -1 for no tint
-   * @param luminosity     Lighting for the part, 0 for no extra lighting
+   * @param emissivity     Emissivity for fullbright, -1 will leave forge in charge, 0-15 will override the forge value
    * @param location       Model location for errors
    * @return  Baked quad
    */
   public static BakedQuad bakeQuad(Vector3f posFrom, Vector3f posTo, BlockElementFace face, TextureAtlasSprite sprite,
                                    Direction facing, Transformation transform, boolean uvlock, @Nullable BlockElementRotation partRotation,
-                                   boolean shade, int color, int luminosity, ResourceLocation location) {
+                                   boolean shade, int emissivity, ResourceLocation location) {
     BlockFaceUV faceUV = face.uv;
     if (uvlock) {
       faceUV = FaceBakery.recomputeUVs(face.uv, facing, transform, location);
@@ -230,9 +256,9 @@ public class ColoredBlockModel extends SimpleBlockModel {
     faceUV.uvs[1] = Mth.lerp(shrinkRatio, faceUV.uvs[1], v);
     faceUV.uvs[3] = Mth.lerp(shrinkRatio, faceUV.uvs[3], v);
 
-    // TODO: could reimplement color as a quad transformer to reduce duplication of face bakery
-    // look into change while looking into luminosity removal
-    int[] vertexData = makeVertices(faceUV, sprite, facing, FACE_BAKERY.setupShape(posFrom, posTo), transform, partRotation, color, luminosity);
+    // call the vanilla face bakery, we will pass in emmisivity and color via quad transformers
+    // note that in prior versions of mantle we reimplemented the face bakery methods to pass in colors to the face baking directly
+    int[] vertexData = FACE_BAKERY.makeVertices(faceUV, sprite, facing, FACE_BAKERY.setupShape(posFrom, posTo), transform, partRotation, shade);
     Direction direction = FaceBakery.calculateFacing(vertexData);
     System.arraycopy(originalUV, 0, faceUV.uvs, 0, originalUV.length);
     if (partRotation == null) {
@@ -240,55 +266,16 @@ public class ColoredBlockModel extends SimpleBlockModel {
     }
     //noinspection UnstableApiUsage  We are replicating the vanilla method, so we call the forge method
     ForgeHooksClient.fillNormal(vertexData, direction);
+
+    // bake final quad
     BakedQuad quad = new BakedQuad(vertexData, face.tintIndex, direction, sprite, shade);
-//    QuadTransformers.settingEmissivity(face.emissivity).processInPlace(quad);
-    // TODO: forge has a hook for emmisive here, we should probably switch to that over our luminosity
-    return quad;
-  }
-
-  /** Clone of the vanilla method with 2 extra parameters */
-  private static int[] makeVertices(BlockFaceUV uvs, TextureAtlasSprite sprite, Direction orientation, float[] posDiv16, Transformation rotationIn, @Nullable BlockElementRotation partRotation, int color, int luminosity) {
-    int[] vertexData = new int[32];
-    for(int i = 0; i < 4; ++i) {
-      bakeVertex(vertexData, i, orientation, uvs, posDiv16, sprite, rotationIn, partRotation, color, luminosity);
+    // use our override if specified, fallback to Forge
+    if (emissivity == -1) {
+      emissivity = face.emissivity;
     }
-    return vertexData;
-  }
-
-  /** Clone of the vanilla method with 2 extra parameters */
-  private static void bakeVertex(int[] vertexData, int vertexIndex, Direction facing, BlockFaceUV blockFaceUVIn, float[] posDiv16, TextureAtlasSprite sprite, Transformation rotationIn, @Nullable BlockElementRotation partRotation, int color, int luminosity) {
-    FaceInfo.VertexInfo vertexInfo = FaceInfo.fromFacing(facing).getVertexInfo(vertexIndex);
-    Vector3f vector3f = new Vector3f(posDiv16[vertexInfo.xFace], posDiv16[vertexInfo.yFace], posDiv16[vertexInfo.zFace]);
-    FACE_BAKERY.applyElementRotation(vector3f, partRotation);
-    FACE_BAKERY.applyModelRotation(vector3f, rotationIn);
-    fillVertex(vertexData, vertexIndex, vector3f, sprite, blockFaceUVIn, color, luminosity);
-  }
-
-  /**
-   * Converts an ARGB color to an ABGR color, as the commonly used color format is not the format colors end up packed into.
-   * This function doubles as its own inverse, not that its needed.
-   * @param color  ARGB color
-   * @return  ABGR color
-   */
-  private static int swapColorRedBlue(int color) {
-    return (color & 0xFF00FF00) // alpha and green same spot
-           | ((color >> 16) & 0x000000FF) // red moves to blue
-           | ((color << 16) & 0x00FF0000); // blue moves to red
-  }
-
-  /** Clone of the vanilla method with 2 extra parameters, major logic changes are in this code */
-  private static void fillVertex(int[] vertexData, int vertexIndex, Vector3f vector, TextureAtlasSprite sprite, BlockFaceUV blockFaceUV, int color, int luminosity) {
-    int i = vertexIndex * 8;
-    // XYZ - 3 ints
-    vertexData[i] = Float.floatToRawIntBits(vector.x());
-    vertexData[i + 1] = Float.floatToRawIntBits(vector.y());
-    vertexData[i + 2] = Float.floatToRawIntBits(vector.z());
-    // color - 1 int in ABGR format, we use ARGB format as that is used everywhere else. vanilla uses -1 here
-    vertexData[i + 3] = swapColorRedBlue(color);
-    // UV - 2 ints
-    vertexData[i + 4] = Float.floatToRawIntBits(sprite.getU((double)blockFaceUV.getU(vertexIndex) * .999 + blockFaceUV.getU((vertexIndex + 2) % 4) * .001));
-    vertexData[i + 5] = Float.floatToRawIntBits(sprite.getV((double)blockFaceUV.getV(vertexIndex) * .999 + blockFaceUV.getV((vertexIndex + 2) % 4) * .001));
-    // light UV - 1 ints, just setting block light here rather than block and sky. vanilla uses 0 here
-    vertexData[i + 6] = (luminosity << 4);
+    if (emissivity > 0) {
+      QuadTransformers.settingEmissivity(emissivity).processInPlace(quad);
+    }
+    return quad;
   }
 }
