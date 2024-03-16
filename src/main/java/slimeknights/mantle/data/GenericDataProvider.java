9922b9e38f42fda0382668de2a16a46a93912491
KnightMiner <knightminer4@gmail.com>
2024-03-16 01:58:05 -0400

    Add a method to GenericDataProvider to save JSON using a codec
    Seems like the type of thing that is going to start coming up frequently

--- src/main/java/slimeknights/mantle/data/GenericDataProvider.java
+++ src/main/java/slimeknights/mantle/data/GenericDataProvider.java
@@ -1,6 +1,8 @@
 package slimeknights.mantle.data;
 
 import com.google.gson.Gson;
+import com.mojang.serialization.Codec;
+import com.mojang.serialization.JsonOps;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import net.minecraft.data.CachedOutput;
@@ -8,6 +10,7 @@ import net.minecraft.data.DataGenerator;
 import net.minecraft.data.DataProvider;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.packs.PackType;
+import slimeknights.mantle.Mantle;
 import slimeknights.mantle.util.JsonHelper;
 
 import java.io.IOException;
@@ -15,6 +18,7 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 
 /** Generic logic to convert any serializable object into JSON. */
+@SuppressWarnings("unused")  // API
 @RequiredArgsConstructor
 @Log4j2
 public abstract class GenericDataProvider implements DataProvider {
@@ -40,5 +44,16 @@ public abstract class GenericDataProvider implements DataProvider {
     } catch (IOException e) {
       log.error("Couldn't create data for {}", location, e);
     }
+  }
+
+  /**
+   * Saves the given object to JSON using a codec
+   * @param output     Output for writing
+   * @param location   Location relative to this data provider's root
+   * @param codec      Codec to save the object
+   * @param object     Object to save, will be converted using the passed codec
+   */
+  protected <T> void saveJson(CachedOutput output, ResourceLocation location, Codec<T> codec, T object) {
+    saveJson(output, location, codec.encodeStart(JsonOps.INSTANCE, object).getOrThrow(false, Mantle.logger::error));
   }
 }
