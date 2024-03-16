0f1877cec6ca9f660de3afd0fd08a7d668f1bf2f
KnightMiner <knightminer4@gmail.com>
2024-03-16 02:04:51 -0400

    Switch fluid ingredient static key to "fluid" instead of "name"
    See #203
    Makes it more conssitent with item ingredients and recipe helper

--- src/main/java/slimeknights/mantle/recipe/ingredient/FluidIngredient.java
+++ src/main/java/slimeknights/mantle/recipe/ingredient/FluidIngredient.java
@@ -197,7 +197,7 @@ public abstract class FluidIngredient {
     }
 
     // fluid match
-    if (json.has("name")) {
+    if (json.has("fluid")) {
       // don't set both, obviously an error
       if (json.has("tag")) {
         throw new JsonSyntaxException("An ingredient entry is either a tag or an fluid, not both");
@@ -300,7 +300,7 @@ public abstract class FluidIngredient {
     @Override
     public JsonElement serialize() {
       JsonObject object = new JsonObject();
-      object.addProperty("name", Registry.FLUID.getKey(fluid).toString());
+      object.addProperty("fluid", Registry.FLUID.getKey(fluid).toString());
       object.addProperty("amount", amount);
       return object;
     }
@@ -320,7 +320,7 @@ public abstract class FluidIngredient {
      * @return Fluid ingredient instance
      */
     private static FluidMatch deserialize(JsonObject json) {
-      String fluidName = GsonHelper.getAsString(json, "name");
+      String fluidName = GsonHelper.getAsString(json, "fluid");
       Fluid fluid = ForgeRegistries.FLUIDS.getValue(new ResourceLocation(fluidName));
       if (fluid == null || fluid == Fluids.EMPTY) {
         throw new JsonSyntaxException("Unknown fluid '" + fluidName + "'");
