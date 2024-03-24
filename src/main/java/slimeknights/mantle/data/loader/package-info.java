87032a1079deca7ebd9c50aa06a24b5d8ec06ade
KnightMiner <knightminer4@gmail.com>
2024-03-23 21:00:26 -0400

    Delete the generic 1 field loaders
    Replaced by RecordLoadable and the various related loadables

--- src/main/java/slimeknights/mantle/data/loader/package-info.java
+++
@@ -1,7 +0,0 @@
-@ParametersAreNonnullByDefault
-@MethodsReturnNonnullByDefault
-package slimeknights.mantle.data.loader;
-
-import net.minecraft.MethodsReturnNonnullByDefault;
-
-import javax.annotation.ParametersAreNonnullByDefault;
