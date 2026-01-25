43e638d842cd68299b9427f85910f98f0d10fe9f
paypur <pieceofpaypur@gmail.com>
2026-01-25 01:07:13 -0500

    Removed unnecessary string

--- src/main/java/slimeknights/mantle/client/book/data/content/PageContent.java
+++ src/main/java/slimeknights/mantle/client/book/data/content/PageContent.java
@@ -174,8 +174,8 @@ public abstract class PageContent implements IHTML {
     if (classes != null) classesBuilder.append(" ").append(classes);
 
     StringBuilder stylesBuilder = new StringBuilder();
-    if (Boolean.TRUE.equals(getCenterTitle())) stylesBuilder.append("align-self: center; ");
-    if (styles != null) stylesBuilder.append("; ").append(styles);
+    if (Boolean.TRUE.equals(getCenterTitle())) stylesBuilder.append("align-self: center;");
+    if (styles != null) stylesBuilder.append(styles);
 
     return HTMLUtils.p(getTitle(), parent.parent.name +  "." + parent.name, classesBuilder.toString(), null, stylesBuilder.isEmpty() ? null : stylesBuilder.toString());
   }
