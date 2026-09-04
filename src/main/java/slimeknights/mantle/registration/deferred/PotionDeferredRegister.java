9e05f5c4925f64cdf523be145d51c3a5c47354c7
KnightMiner <knightminer4@gmail.com>
2026-09-04 01:51:37 -0400

    Add a TODO comment to potion register reminding me to fix the registry keys

--- src/main/java/slimeknights/mantle/registration/deferred/PotionDeferredRegister.java
+++ src/main/java/slimeknights/mantle/registration/deferred/PotionDeferredRegister.java
@@ -63,6 +63,7 @@ public class PotionDeferredRegister extends DeferredRegisterWrapper<Potion> {
 
     /** Adds the given potion type */
     private Builder with(PotionType type, int duration, int amplifier) {
+      // TODO 1.21: normal should have no prefix, not just an underscore. Move the underscore addition to line below
       String prefix = type == PotionType.NORMAL ? "" : type.toString().toLowerCase(Locale.ROOT);
       builder.put(type, register(prefix + '_' + name, () -> new Potion(modID + "." + name, new MobEffectInstance(effect.get(), duration, amplifier))));
       return this;
