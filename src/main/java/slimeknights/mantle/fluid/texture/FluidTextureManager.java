e4225041497ad5074b3f4d6f0ef31f47c1c26443
KnightMiner <knightminer4@gmail.com>
2026-09-18 17:28:52 -0400

    Ensure a fluid texture failing to parse doesn't cause others to not be loaded

--- src/main/java/slimeknights/mantle/fluid/texture/FluidTextureManager.java
+++ src/main/java/slimeknights/mantle/fluid/texture/FluidTextureManager.java
@@ -59,8 +59,12 @@ public class FluidTextureManager implements IEarlySafeManagerReloadListener {
       if (type == null || !id.equals(fluidTypeRegistry.getKey(type))) {
         Mantle.logger.debug("Ignoring fluid texture {} as no fluid type exists with that name", id);
       } else {
-        // parse it if valid
-        map.put(type, FluidTexture.deserialize(GsonHelper.convertToJsonObject(entry.getValue(), "fluid_texture")));
+        try {
+          // parse it if valid
+          map.put(type, FluidTexture.deserialize(GsonHelper.convertToJsonObject(entry.getValue(), "fluid_texture")));
+        } catch (RuntimeException e) {
+          Mantle.logger.error("Failed to deserialize fluid texture {}", id, e);
+        }
       }
     }
     this.textures = map;
